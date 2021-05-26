45c34a031b0058e8a51ff829cfb7009befee87fc
tlcl7426 <1916782641@qq.com>
2021-05-26 22:56:11 +0800

    vuereact学习

--- app.js
+++ app.js
@@ -1,61 +1,61 @@
-// let current;
+let current;
 
-// class Obser {
-//   constructor(age, name) {
-//     this.age = age;
-//     this.name = name;
-//   }
-// }
-// class Dep {
-//   constructor() {
-//     this.sub = new Set();
-//   }
-//   depend() {
-//     if (current) {
-//       this.sub.add(current);
-//     }
-//   }
-//   notify() {
-//     this.sub.forEach((el) => {
-//       el();
-//       console.log(el);
-//     });
-//   }
-// }
-// let ob = new Obser(1, 2);
-// let omap = new Map();
-// // let de = new Dep();
+class Obser {
+  constructor(age, name) {
+    this.age = age;
+    this.name = name;
+  }
+}
+class Dep {
+  constructor() {
+    this.sub = new Set();
+  }
+  depend() {
+    if (current) {
+      this.sub.add(current);
+    }
+  }
+  notify() {
+    this.sub.forEach((el) => {
+      el();
+      console.log(el);
+    });
+  }
+}
+let ob = new Obser(1, 2);
+let omap = new Map();
 // let de = new Dep();
-// function reactive(raw) {
-//   return new Proxy(raw, {
-//     get(target, key) {
-//       de.depend();
-//       return Reflect.get(target, key);
-//     },
-//     set(target, key, value) {
-//       let re = Reflect.set(target, key, value);
-//       // console.log(de);
-//       de.notify();
+let de = new Dep();
+function reactive(raw) {
+  return new Proxy(raw, {
+    get(target, key) {
+      de.depend();
+      return Reflect.get(target, key);
+    },
+    set(target, key, value) {
+      let re = Reflect.set(target, key, value);
+      // console.log(de);
+      de.notify();
 
-//       return re;
-//     },
-//   });
-// }
+      return re;
+    },
+  });
+}
 
-// function watch(effect) {
-//   current = effect;
-//   effect();
-//   current = null;
-// }
+function watch(effect) {
+  current = effect;
+  effect();
+  current = null;
+}
 
-// const user = reactive(ob);
+const user = reactive(ob);
 
-// watch(() => {
-//   console.log(user.age);
-// });
+watch(() => {
+  console.log(user.age);
+});
 
-// watch(() => {
-//   console.log(user.name);
-// });
-// user.age = 1;
-// user.name = 3;
+watch(() => {
+  console.log(user.name);
+});
+user.age = 1;
+user.name = 3;
